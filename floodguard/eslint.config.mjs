2b44b6e623909ee664e6b39c9d7b0a865abaf6f3
Alok Agrahari <advancedtechnology62@gmail.com>
2026-02-02 13:03:48 +0530

    2.9_Typescript

--- floodguard/eslint.config.mjs
+++ floodguard/eslint.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettierConfig, // Sabse niche hona chahiye taaki rules override na ho
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error", // 'any' use karna mana hai
+      "no-unused-vars": "warn",
+      "no-console": "warn",       
+      "semi": ["error", "always"], 
+      "quotes": ["error", "double"] 
+    }
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
